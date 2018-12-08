94f6699cfde70f463c26935a732c42de72ea1a5c
imrajjain <imrajjain2109@gmail.com>
2018-12-08 20:59:14 +0530

    form usability

--- js/main.js
+++ js/main.js
@@ -2,6 +2,6 @@ function postContactToGoogle() {
     var rqst=$('#rqst').val();
     
     $.ajax({
-    url:"https://docs.google.com/forms/d/e/1FAIpQLSfEZXpSmTbps38ODxLMzHFPy8PZpbE87icHND6wmjSlGQDv-w/formResponse", data:{"entry_259469016":rqst},type:"POST",dataType:"xml",statusCode: {0:function() { window.location.replace("index.html");},200:function(){window.location.replace("index.html");}}
+    url:"https://docs.google.com/forms/d/e/1FAIpQLSfEZXpSmTbps38ODxLMzHFPy8PZpbE87icHND6wmjSlGQDv-w/formResponse", data:{"entry.259469016":rqst},type:"POST",dataType:"xml",statusCode: {0:function() { window.location.replace("index.html");},200:function(){window.location.replace("index.html");}}
     });
     }
