0fdaebd4cf9bc11181b4af7111aebd5a1ab1b1ca
imrajjain <imrajjain2109@gmail.com>
2018-12-08 20:47:02 +0530

    Added form usability

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function postContactToGoogle() {
+    var rqst=$('#rqst').val();
+    
+    $.ajax({
+    url:"https://docs.google.com/forms/d/e/1FAIpQLSfEZXpSmTbps38ODxLMzHFPy8PZpbE87icHND6wmjSlGQDv-w/formResponse",
+    data:{"entry_259469016":fullname},type:"POST",dataType:"xml",statusCode: {0:function() { window.location.replace("index.html");},200:function(){window.location.replace("index.html");}}
+    });
+    }
